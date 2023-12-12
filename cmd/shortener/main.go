7ecfb65316cab4d60f17e3d71547f0659810035a
Aigars Beinarovics <a.beinarovics@gmail.com>
2023-12-12 08:46:07 +0200

    Add ENV support for server configuration

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/f0zze/shorter/internal/app/storage"
 	chi2 "github.com/go-chi/chi/v5"
 	"net/http"
+	"os"
 )
 
 type ServerConfig struct {
@@ -19,10 +20,20 @@ func getConfig() ServerConfig {
 	destHost := flag.String("b", "http://localhost:8080", "Response server URL")
 	flag.Parse()
 
-	return ServerConfig{
+	config := ServerConfig{
 		*host,
 		*destHost,
 	}
+
+	if envRunAdd := os.Getenv("SERVER_ADDRESS"); envRunAdd != "" {
+		config.host = envRunAdd
+	}
+
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		config.response = envBaseURL
+	}
+
+	return config
 }
 
 func main() {
