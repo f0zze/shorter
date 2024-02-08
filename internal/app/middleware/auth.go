43f2d7048ff6a793e04760e29a251f6f1f26e418
Aigars Beinarovics <a.beinarovics@gmail.com>
2024-02-08 20:33:22 +0200

    Set auth cookie for user with no cookie :)

--- internal/app/middleware/auth.go
+++ internal/app/middleware/auth.go
@@ -3,37 +3,39 @@ package middleware
 import (
 	"context"
 	"github.com/f0zze/shorter/internal/app"
+	"github.com/f0zze/shorter/internal/app/services"
 	"net/http"
+	"time"
 )
 
 func WithAuth() func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
-			//tokenString, err := r.Cookie("Authorization")
-			//
-			//if err != nil {
-			//	newUserID := services.NewUUID()
-			//	token, err := services.BuildJWTString(newUserID)
-			//
-			//	if err != nil {
-			//		w.WriteHeader(http.StatusInternalServerError)
-			//		return
-			//	}
-			//
-			//	cookie := http.Cookie{
-			//		Name:     "Authorization",
-			//		Value:    token,
-			//		Expires:  time.Now().Add(24 * time.Hour),
-			//		HttpOnly: true,
-			//		Path:     "/",
-			//	}
-			//
-			//	http.SetCookie(w, &cookie)
-			//	ctx := context.WithValue(r.Context(), app.UserIDContext, newUserID)
-			//	next.ServeHTTP(w, r.WithContext(ctx))
-			//
-			//	return
-			//}
+			_, err := r.Cookie("Authorization")
+
+			if err != nil {
+				newUserID := services.NewUUID()
+				token, err := services.BuildJWTString(newUserID)
+
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				cookie := http.Cookie{
+					Name:     "Authorization",
+					Value:    token,
+					Expires:  time.Now().Add(24 * time.Hour),
+					HttpOnly: true,
+					Path:     "/",
+				}
+
+				http.SetCookie(w, &cookie)
+				ctx := context.WithValue(r.Context(), app.UserIDContext, newUserID)
+				next.ServeHTTP(w, r.WithContext(ctx))
+
+				return
+			}
 			//
 			//userID, err := services.GetUserID(tokenString.Value)
 			//
